60aa882a9293462391e31edd4c6b96cdf552f81f
Parasuramandckap <parasuaptadckap@gmail.com>
2023-06-19 16:19:17 +0530

    product create and list operation using php

--- views/product/edit.php
+++ views/product/edit.php
@@ -8,7 +8,7 @@
     </head>
     <body>
         <?php foreach ($read as $reads):?>
-        <h1>please change change sku in before update</h1>
+        <h1>please change  sku in before update</h1>
         <section class="bg-gray-50 dark:bg-gray-900">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto  lg:py-0">
                 <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
